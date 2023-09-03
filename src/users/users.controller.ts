9f944557879919c50e397dba25ebe0cdc4ee858c
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2023-09-03 21:32:06 +0600

    added serialize interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -22,6 +24,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(parseInt(id));
